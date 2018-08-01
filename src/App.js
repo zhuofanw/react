dc36340654be2c0627ee1cf4182e5222987a4ed3
zhuofan <zhuofan_wei@outlook.com>
2018-07-31 22:15:07 -0700

    react-project stage 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Header} from './Header'
+import {Main} from './Main'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
           <Header/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Main/>
       </div>
     );
   }
