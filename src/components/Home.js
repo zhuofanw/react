8d4045ea36925eafb0e671d23848753fcac1e8a1
zhuofanw <zhuofan_wei@outlook.com>
2018-08-07 19:42:07 -0700

    tab created

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Tabs, Button } from 'antd';
+
+const TabPane = Tabs.TabPane;
+
+const operations = <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
     render() {
         return (
-            <div>this is home</div>
+            <Tabs tabBarExtraContent={operations} className="main-tabs">
+                <TabPane tab="Posts" key="1">Content of tab 1</TabPane>
+                <TabPane tab="Map" key="2">Content of tab 2</TabPane>
+            </Tabs>
         );
     }
 }
