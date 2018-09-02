e14ee4869718498e073ab624992577511973186e
zhuofanw <zhuofan_wei@outlook.com>
2018-09-02 15:04:56 -0700

    MouseOver/out InfoWindow show/hide

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {withScriptjs,
     withGoogleMap,
     GoogleMap,
-    Marker,
 } from "react-google-maps";
 import {POS_KEY} from "../constants"
+import {AroundMarker} from "./AroundMarker"
 
 class AroundMap extends React.Component{
     render(){
@@ -14,12 +14,8 @@ class AroundMap extends React.Component{
                 defaultZoom={8}
                 defaultCenter={{ lat, lng: lon }}
             >
-                <Marker
-                    position={{ lat, lng: lon + 0.01 }}
-                />
-                <Marker
-                    position={{ lat: lat + 0.01, lng: lon - 0.01 }}
-                />
+                <AroundMarker position={{lat:lat + 0.01, lon: lon + 0.01}}/>
+                <AroundMarker position={{ lat: lat + 0.01, lon: lon - 0.01 }}/>
             </GoogleMap>
         );
     }
---
+++ src/components/AroundMarker.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Marker,InfoWindow} from "react-google-maps"
+
+export class AroundMarker extends React.Component{
+    state = {
+        isOpen: false,
+    }
+
+    onToggleOpen = () => {
+        this.setState((prevState) => {
+            return {
+                isOpen: !prevState.isOpen,
+            };
+        });
+    }
+    render(){
+        const {lat, lon} = this.props.position;
+        return(
+            <Marker
+                position={{ lat, lng: lon }}
+                onMouseOver={this.onToggleOpen}
+                onMouseOut={this.onToggleOpen}>
+            {this.state.isOpen ?
+                <InfoWindow>
+                <div>content</div>
+                </InfoWindow>
+                : null
+                }
+        </Marker>
+        );
+    }
+}
